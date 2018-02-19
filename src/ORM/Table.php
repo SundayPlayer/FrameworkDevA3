142e5135499c470668488a1bdc87e4bc0d6a2d81
Baptiste Vadé <sundayplayer@outlook.fr>
2018-02-19 14:07:29 +0100

    générating Table and Entity class

---
+++ src/ORM/Table.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace FrameworkDevA3\ORM;
+
+class Table
+{
+    private $name;
+
+    private $entity;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return Entity
+     */
+    public function getEntity()
+    {
+        return $this->entity;
+    }
+
+    /**
+     * @param Entity $entity
+     */
+    public function setEntity($entity)
+    {
+        $this->entity = $entity;
+    }
+}
