8de4a06c47434862ee07a12689cc022c1f576270
DavcoDev <david.cossedu@gmail.com>
2018-02-19 14:29:24 +0100

    ajout classe ModelTable and ModelEntity

---
+++ src/Models/ModelEntity.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: David C
+ * Date: 19/02/2018
+ * Time: 14:25
+ */
+
+class ModelEntity extends Model
+{
+    protected $data;
+    /**
+     * ModelEntity constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function validate()
+    {
+
+    }
+
+    public function save()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
