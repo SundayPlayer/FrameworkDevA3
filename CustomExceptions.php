761c0ec42fb8ab594aba1315bd8425b93a878196
donovan <donovan.fremy@gmail.com>
2018-02-19 14:32:45 +0100

    add Custom test.php

--- CustomExceptions.php
+++ CustomExceptions.php
@@ -1,5 +1,11 @@
 <?php
 class CustomExceptions extends Exception {
+	public function __construct($message, $code=0 ) {
+		parent::__construct($message, $code);
+	}
 
+	public function __toString() {
+		return $this->message;
+	}
 }
 
---
+++ test.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: donovan
+ * Date: 19/02/2018
+ * Time: 14:01
+ */
+function additionner( $a, $b ) {
+	if ( ! is_numeric( $a ) || ! is_numeric( $b ) ) {
+		throw new Exception( 'Les deux paramètres doivent être des nombres' ); // On lance une exception "MonException".
+	}
+	return $a + $b;
+}
+try // Nous allons essayer d'effectuer les instructions situées dans ce bloc.
+{
+	echo additionner( 12, 3 ), '<br />';
+	echo additionner( 'azerty', 54 ), '<br />';
+	echo additionner( 4, 8 );
+
+} catch ( Exception $e ) // Nous allons attraper les exceptions "MonException" s'il y en a une qui est levée.
+
+{
+	echo $e; // On affiche le message d'erreur grâce à la méthode __toString que l'on a écrite.
+}
+echo '<br />Fin du script'; // Ce message s'affiche, ça prouve bien que le script est exécuté jusqu'au bout.
