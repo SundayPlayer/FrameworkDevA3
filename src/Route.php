147818b0c3f3daf2568d2b980e237d8a080c9d8e
Erwan L <gnawre50@gmail.com>
2018-02-19 16:57:24 +0100

    Controller call ?

--- src/Route.php
+++ src/Route.php
@@ -31,7 +31,7 @@ class Route
     public function call(){
         if(is_string($this->function)){
             $params = explode('#', $this->function);
-            $controller = require_once "src\\".$params[0]."Controller";
+            $controller = "src\\".$params[0]."Controller";
             $controller = new $controller();
             return call_user_func_array([$controller, $params[1]], $this->matches);
         } else {
