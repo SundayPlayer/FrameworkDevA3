846146b27726e515070a70304f0d2e07fb8ba869
Camille Breuils <breuils.camille@gmail.com>
2018-02-19 15:11:33 +0100

    call

--- src/Route.php
+++ src/Route.php
@@ -2,19 +2,21 @@
 
 class Route
 {
-
+    // PROPRIETES
     private $path;
     private $function;
     private $matches = [];
 
+    // CONSTRUCTEUR
     public function __construct($path, $function)
     {
         $this->path = trim($path, '/');
         $this->function = $function;
     }
 
+    // FONCTIONS
     public function match($url){
-        $url = trim($url, '/');
+        $url = trim($url, '/'); // suppression du / de fin
         $path = preg_replace('#:([\w]+)#', '([^/]+)', $this->path);
         $regex = "#^$path$#i";
         if(!preg_match($regex, $url, $matches)){
@@ -25,4 +27,16 @@ class Route
             return true;
         }
     }
+
+    public function call(){
+//        if(is_string($this->function)){
+//            $params = explode('#', $this->function);
+//            $controller = "src\\" . $params[0] . "Controller";
+//            $controller = new $controller();
+//            return call_user_func_array([$controller, $params[1]], $this->matches);
+//        } else {
+            return call_user_func_array($this->function, $this->matches);
+       // }
+    }
+
 }
