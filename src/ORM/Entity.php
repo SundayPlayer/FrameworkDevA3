4d2b7ade8d548c080fb9ce42531474bafad1323e
Baptiste Vadé <sundayplayer@outlook.fr>
2018-02-19 14:07:29 +0100

    générating Table and Entity class

---
+++ src/ORM/Entity.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FrameworkDevA3\ORM;
+
+class Entity
+{
+    private $data;
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param mixed $data
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+}
