7af5274fa7a6c4513104d1e974c14ef637816a9f
Erwan L <gnawre50@gmail.com>
2018-02-19 15:13:59 +0100

    Routes

--- src/Router.php
+++ src/Router.php
@@ -1,4 +1,7 @@
 <?php
+
+include 'Route.php';
+
 class Router {
 
     private $url; // contiendra l'URL sur laquelle on veut aller
@@ -9,8 +12,24 @@ class Router {
         echo $this->url;
     }
 
+    // Permet l'ajout d'une route basique, à partir d'un URL et d'une fonction ou d'un controller à appeler.
+    // Retourne la route prête.
+    public function get($path, $function){
+        $route = new Route($path, $function);
+        $this->routes["GET"][] = $route;
+        return $route;
+    }
 
-
+    public function run(){
+        if(!isset($this->routes[$_SERVER['REQUEST_METHOD']])){
+            return false;
+        }
+        foreach ($this->routes[$_SERVER['REQUEST_METHOD']] as $route){
+            if($route->match($this->url)){
+                return $route->call();
+            }
+        }
+    }
 
 
 }
