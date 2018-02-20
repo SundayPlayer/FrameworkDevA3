5a0f06ecced412dd442e45f5cb3a8d5b68bba594
Baptiste Vadé <sundayplayer@outlook.fr>
2018-02-20 14:02:21 +0100

    orm en l'état

--- src/ORM/Table.php
+++ src/ORM/Table.php
@@ -3,44 +3,66 @@
 namespace FrameworkDevA3\ORM;
 
 use FrameworkDevA3\ORM\Traits\TableTrait;
+use FrameworkDevA3\Utility\Str;
 
 class Table
 {
-    use TableTrait;
+    protected $tableName;
 
-    private $name;
+    protected $entityName;
 
-    private $entities;
+    protected $entities = [];
 
-    /**
-     * @return string
-     */
-    public function getName()
+    public function __call(string $name, array $arguments)
     {
-        return $this->name;
-    }
+        $db = Core::db();
 
-    /**
-     * @param string $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
+        $data = [];
+        // Split du name à toute les majuscules
+        $arr = preg_split('/(?=[A-Z])/', $name, -1, PREG_SPLIT_NO_EMPTY);
 
-    /**
-     * @return Entity[]
-     */
-    public function getEntities()
-    {
-        return $this->entities;
-    }
+        $entityClassName = Str::entityName($this->entityName, true);
 
-    /**
-     * @param Entity[] $entities
-     */
-    public function setEntities($entities)
-    {
-        $this->entities = $entities;
+        switch (count($arr)) {
+            case 1:
+                break;
+
+            // getAll
+            case 2:
+                if ($arr[0] === 'get' && $arr[1] === 'All') {
+                    $sth = $db->prepare("SELECT * FROM $this->tableName");
+                    $sth->execute();
+                    $data = $sth->fetchAll(\PDO::FETCH_CLASS, $entityClassName);
+                }
+                break;
+
+            // getByField
+            case 3:
+                if ($arr[0] === 'get' && $arr[1] === 'By') {
+                    $sth = $db->prepare("SELECT * FROM $this->tableName WHERE {$arr[2]} = '{$arguments[0]}'");
+                    $sth->execute();
+                    $data = $sth->fetchAll(\PDO::FETCH_CLASS, $entityClassName);
+                }
+                break;
+
+            // getByFieldOrderBy
+            case 5:
+                if ($arr[0] === 'get' && $arr[1] === 'By' && $arr[3] === 'Order' && $arr[4] === 'By') {
+                    $sth = $db->prepare("
+                                    SELECT * 
+                                    FROM $this->tableName 
+                                    WHERE {$arr[2]} = '{$arguments[0]}'
+                                    ORDER BY {$arguments[1]} {$arguments[2]}");
+                    $sth->execute();
+                    $data = $sth->fetchAll(\PDO::FETCH_CLASS, $entityClassName);
+                }
+                break;
+
+            default:
+                break;
+        }
+        $this->entities = $data;
+
+        return $this;
     }
 }
