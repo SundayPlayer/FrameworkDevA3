f5dd55a859c59910398dd1e547cd1fd164aab084
allan <all.brochard@gmail.com>
2018-02-20 13:53:02 +0100

    Add function Entity ORM

--- src/ORM/Entity.php
+++ src/ORM/Entity.php
@@ -2,6 +2,8 @@
 
 namespace FrameworkDevA3\ORM;
 
+use ArrayIterator;
+use FrameworkDevA3\ORM\Core;
 use FrameworkDevA3\ORM\Traits\EntityTrait;
 
 class Entity
@@ -24,5 +26,45 @@ class Entity
     public function setData($data)
     {
         $this->data = $data;
+    }
+
+
+    public function create()
+    {
+        $db = Core::db();
+        $data = new ArrayIterator($this->data);
+
+        $values="";
+        foreach ($data as $key => $value){
+            $values.=$value;
+            if($data->hasNext()){
+                $values.=", ";
+            }
+        }
+        $query = $db->query("INSERT INTO ".$this->name." VALUES (".$values.")");
+        $query->fetch();
+    }
+
+    public function update()
+    {
+        $db = Core::db();
+        $data = new ArrayIterator($this->data);
+
+        $values="";
+        foreach ($data as $key => $value){
+            $values.=$key." = ".$value;
+            if($data->hasNext()){
+                $values.=", ";
+            }
+        }
+        $query = $db->query("UPDATE ".$this->name."SET ".$data." WHERE id=".$this->id);
+        $query->fetch();
+    }
+
+    public function delete()
+    {
+        $db = Core::db();
+        $query = $db->query("DELETE FROM ". $this->name." WHERE id=".$this->id);
+        $query->fetch();
     }
 }
