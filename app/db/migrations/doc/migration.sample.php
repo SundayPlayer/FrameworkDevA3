d37f6144fe2d4e6476084e0a2620b4ca235a2e36
Luc <luck.pieri@gmail.com>
2018-02-22 12:26:54 +0100

    added doc to app/db/migrations

--- app/db/migrations/doc/migration.sample.php
+++ app/db/migrations/doc/migration.sample.php
@@ -4,14 +4,14 @@ require_once '../../src/Migration/BaseMigration.php';
 
 use FrameworkDevA3\Migration\BaseMigration;
 
-class CreatePostsTable extends BaseMigration
+class CreateSampleTable extends BaseMigration
 {
     public function up() {
         $this->path = __FILE__;
 
-        $this->table('posts')
-            ->addColumn('excerpt', 'string')
-            ->addColumn('content', 'text')
+        $this->table('tableName')
+            ->addColumn('columnName', 'string')
+            ->addColumn('columnName', 'text')
             ->create();
     }
 }
